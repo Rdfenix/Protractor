761779976285368eda78da0bd777c576978d839a
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-04-27 14:10:42 -0300

    Estudo sobre Protractor

--- spec/spec.js
+++ spec/spec.js
@@ -3,6 +3,7 @@ describe('Estudo sobre Protractor', function() {
 		// pega a url e adicionar no browser
 		browser.get('http://juliemr.github.io/protractor-demo/');
 		//pega o titulo e verifica se o titulo é igual ao passado pela comparador toEqual.
+		//comparador do campo <title>qualquer coisa</title> pgando valor dentro do  <head></head>
 		expect(browser.getTitle()).toEqual('Super Calculator');
 	});
 });

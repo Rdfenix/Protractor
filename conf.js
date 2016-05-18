02b5e8684da28d3e93a661f26baad2d75b03b33d
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-05-18 16:52:13 -0300

    Estudo sobre Protractor

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 exports.config = {
 	framework: 'jasmine',
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['spec/spec5.js'],
+	specs: ['spec/spec6.js'],
 	multiCapabilities: [{
     browserName: 'firefox'
   }, {
--- spec/spec6.js
+++ spec/spec6.js
@@ -1,3 +1,25 @@
+
 describe("Calcular mais de uma operação matematica", function(){
+      var firstNumber = element(by.model('first'));
+      var secondNumber = element(by.model('second'));
+      var goButton = element(by.id('goButton'));
+      var latestResult = element(by.binding('latest'));
+      //var operation = element(by.model('operator').$('[value="DIVISION"]'));
+      var operation = element.all(by.model('operator'));
+      beforeEach(function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+      });
+
+      function add(a, b) {
+          firstNumber.sendKeys(a);
+          operation.get('[value="DIVISION"]').click();
+          secondNumber.sendKeys(b);
+          goButton.click();
+      }
+
+      it("Realizar a Divisao dos numeros", function(){
+        add(2,2);
+        expect(latestResult.getText()).toEqual('1');
+      });
 
 });
